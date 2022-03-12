d6dddcf52061f59c1c821e4a19e085364998a132
Sami E <blue3eagle3naro@gmail.com>
2022-03-12 15:14:17 +0100

    Added more functionality to User

--- permissions.py
+++ permissions.py
@@ -6,3 +6,7 @@ class PermissionName(permissions.BasePermission):
 
     def has_permission(self, request, view):
       return request.user.is_authenticated
+
+class IsAddedByUser(permissions.BasePermission):
+  def has_object_permission(self, request, view, obj):
+    return bool(obj.user == request.user or request.user.is_staff)
--- user/serializer.py
+++ user/serializer.py
@@ -1,7 +1,12 @@
 from user.models import User
 from rest_framework import serializers
 
-class RegistrationSerializer(serializers.ModelSerializer):
+class PublicUserSerializer(serializers.ModelSerializer):
  class Meta:
   model = User
-  fields = ['username', 'email', 'password']
+  exclude = ['password', 'first_name', 'last_name', 'last_login']
+
+class UserSerializer(serializers.ModelSerializer):
+ class Meta:
+  model = User
+  include = '__all__'
