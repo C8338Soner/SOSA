852c06bbba3c203dc6e753b7b43bf44a0733450a
Sami E <blue3eagle3naro@gmail.com>
2022-04-03 16:12:17 +0200

    PostLike ve PostSave view'lerine unique url verdim (unutmusum)

--- post/urls.py
+++ post/urls.py
@@ -4,6 +4,6 @@ from .views import PostCRUD, PostView, PostLike, PostSave
 urlpatterns = [
   path('', PostView.as_view()),
   path('<int:pk>/', PostCRUD.as_view()),
-  path('<int:pk>/', PostLike, name="like"),
-  path('<int:pk>/', PostSave, name="save"),
+  path('<int:pk>/like/', PostLike, name="like"),
+  path('<int:pk>/save/', PostSave, name="save"),
 ]
