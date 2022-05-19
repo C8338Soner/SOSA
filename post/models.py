a1ee8958820c1b475a0a348f22083f733435d27b
Sami E <blue3eagle3naro@gmail.com>
2022-05-19 19:22:04 +0200

    added default deleted user to post publisher

--- post/models.py
+++ post/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+#FUNCTIONS
+def deleted_user():
+  from user.models import User #circular import avoided
+  return User.objects.get_or_create(username="Deleted", is_active=False, id=0)[0]
+
+#MODELS
 class Post(models.Model):
   title=models.CharField(max_length=100)
   content=models.TextField(max_length=100000)
@@ -7,8 +13,9 @@ class Post(models.Model):
   post_date=models.DateTimeField(auto_now_add=True)
   views=models.IntegerField(default=0)
   likes=models.ManyToManyField('user.User', blank=True, related_name='post_likes')
-  publisher = models.ForeignKey('user.User',on_delete=models.SET_DEFAULT, default="Deleted", related_name='post_publisher')
-  status=models.CharField(max_length=30, choices=(("draft","Draft"),("published","Published")),default=("draft","Draft"))
+  publisher = models.ForeignKey('user.User',on_delete=models.SET_DEFAULT, default=deleted_user, related_name='post_publisher')
+  status=models.CharField(max_length=30, 
+    choices=(("draft","Draft"),("published","Published")),default=("draft","Draft"))
 
   def __str__(self):
     return f"{self.title} -- id: {self.id}"
