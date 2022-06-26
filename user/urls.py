39bfac13c809c44aa6081bbf428a234ce8ee797f
Emiktra <blue3eagle3naro@gmail.com>
2022-06-26 13:51:56 +0200

    post working

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from .views import UserView, logout_view, UserCRUD, currentUser
 
 urlpatterns = [
  path('login/', obtain_auth_token, name='login'),
- path('register/', UserView.as_view()),
+ path('', UserView.as_view()),
  path('<int:pk>/', UserCRUD.as_view()),
  path('logout/', logout_view, name='logout'),
  path('currentuser/<str:token>', currentUser, name='currentuser'),
